78e4059a1f3b46fb069fbfe8edf21909dd6fcdf5
Danko Lučić <dlucic33@gmail.com>
2017-07-30 12:26:35 +0200

    enabled eslint to always run before tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "extends": "airbnb-base",
     "env": {
         "mocha": true,
-        "node": true
+        "node": true,
+        "es6": true
     },
     "rules": {
         "no-use-before-define": ["error", { "functions": false, "classes": true }]
