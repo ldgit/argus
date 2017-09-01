61854558999399025ce9493ec4596c52444de44a
Danko Lučić <dlucic33@gmail.com>
2017-09-01 21:40:11 +0200

    modified default configuration for full backward compatibility

--- src/configuration-reader.js
+++ src/configuration-reader.js
@@ -13,6 +13,30 @@ module.exports = function ConfigurationReader() {
         arguments: [],
         testRunnerCommand: 'vendor/bin/phpunit',
       },
+      {
+        extension: 'php',
+        testNameSuffix: 'Test',
+        testDir: 'tests',
+        sourceDir: '',
+        arguments: [],
+        testRunnerCommand: 'vendor/bin/phpunit',
+      },
+      {
+        extension: 'php',
+        testNameSuffix: 'Test',
+        testDir: 'test/unit',
+        sourceDir: '',
+        arguments: [],
+        testRunnerCommand: 'vendor/bin/phpunit',
+      },
+      {
+        extension: 'php',
+        testNameSuffix: 'Test',
+        testDir: 'test',
+        sourceDir: '',
+        arguments: [],
+        testRunnerCommand: 'vendor/bin/phpunit',
+      },
     ],
   };
 
--- test/configuration-reader.test.js
+++ test/configuration-reader.test.js
@@ -13,6 +13,30 @@ describe('ConfigurationReader', () => {
         arguments: [],
         testRunnerCommand: 'vendor/bin/phpunit',
       },
+      {
+        extension: 'php',
+        testNameSuffix: 'Test',
+        testDir: 'tests',
+        sourceDir: '',
+        arguments: [],
+        testRunnerCommand: 'vendor/bin/phpunit',
+      },
+      {
+        extension: 'php',
+        testNameSuffix: 'Test',
+        testDir: 'test/unit',
+        sourceDir: '',
+        arguments: [],
+        testRunnerCommand: 'vendor/bin/phpunit',
+      },
+      {
+        extension: 'php',
+        testNameSuffix: 'Test',
+        testDir: 'test',
+        sourceDir: '',
+        arguments: [],
+        testRunnerCommand: 'vendor/bin/phpunit',
+      },
     ],
   };
 
