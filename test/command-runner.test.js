dc205da684642ff87a1a423005bd001e9004dac8
Danko Lučić <dlucic33@gmail.com>
2017-09-03 21:50:20 +0200

    modified command runner module * now accepts a list of commands * added synchronous implementation

--- test/command-runner.test.js
+++ test/command-runner.test.js
@@ -1,51 +1,63 @@
 const assert = require('assert');
 const clock = require('lolex');
-const CommandRunner = require('../src/command-runner');
+const CommandRunner = require('../src/command-runner').class;
 const nullPrinter = require('../src/printer').createNull();
+const commandRunnerSyncImpl = require('../src/command-runner').getSynchronousImplementation(nullPrinter);
+
+describe('command-runner synchronous implementation', () => {
+  it('smoke test', () => {
+    commandRunnerSyncImpl.run([{ command: 'echo', args: [] }]);
+  });
+});
 
 describe('command-runner', () => {
   let commandRunner;
   let spawnSpyData;
+  let spawnSpyWasCalled;
   const spawnSpy = (command, args, options) => {
-    spawnSpyData.wasCalled = true;
-    spawnSpyData.lastCommand = command;
-    spawnSpyData.lastArgs = args;
-    spawnSpyData.lastOptions = options;
+    spawnSpyWasCalled = true;
+    spawnSpyData.push({ command, args, options });
   };
 
   beforeEach(() => {
+    spawnSpyData = [];
+    spawnSpyWasCalled = false;
     commandRunner = new CommandRunner(spawnSpy, nullPrinter);
-    spawnSpyData = { lastCommand: null, lastArgs: null, lastOptions: null, wasCalled: false };
   });
 
-  it('should run given command', () => {
-    commandRunner.run({ command: 'echo', args: ['what if this was a unit test command?'] });
-    assert.equal(spawnSpyData.lastCommand, 'echo');
-    assert.deepEqual(spawnSpyData.lastArgs, ['what if this was a unit test command?']);
-    assert.equal(spawnSpyData.lastOptions.stdio, 'inherit');
+  it('should run given commands', () => {
+    commandRunner.run([
+      { command: 'echo', args: ['what if this was a unit test command?'] },
+      { command: 'ls', args: ['-lah'] },
+    ]);
+    assert.equal(spawnSpyData[0].command, 'echo');
+    assert.equal(spawnSpyData[1].command, 'ls');
+    assert.deepEqual(spawnSpyData[0].args, ['what if this was a unit test command?']);
+    assert.deepEqual(spawnSpyData[1].args, ['-lah']);
+    assert.equal(spawnSpyData[0].options.stdio, 'inherit');
+    assert.equal(spawnSpyData[1].options.stdio, 'inherit');
   });
 
-  it('should not run null commands', () => {
-    commandRunner.run({ command: '', args: [''] });
-    assert.strictEqual(spawnSpyData.wasCalled, false);
-
-    commandRunner.run({ command: '', args: ['neki argument'] });
-    assert.strictEqual(spawnSpyData.wasCalled, false);
+  it('should do nothing if given empty array', () => {
+    commandRunner.run([]);
+    assert.strictEqual(spawnSpyWasCalled, false);
   });
 
   context('when running command', () => {
     let textSentToInfo;
+
     beforeEach(() => {
-      textSentToInfo = '';
+      textSentToInfo = [];
       nullPrinter.info = (text) => {
-        textSentToInfo = text;
+        textSentToInfo.push(text);
       };
     });
 
     it('should print info message', () => {
       clock.install({ now: new Date(2017, 7, 1, 18, 5, 5) });
-      commandRunner.run({ command: 'echo', args: ['some arg'] });
-      assert.equal(textSentToInfo, '[2017-08-01 18:05:05] echo "some arg"');
+      commandRunner.run([{ command: 'echo', args: ['one'] }, { command: 'echo', args: ['two'] }]);
+      assert.equal(textSentToInfo[0], '[2017-08-01 18:05:05] echo "one"');
+      assert.equal(textSentToInfo[1], '[2017-08-01 18:05:05] echo "two"');
     });
   });
 });
